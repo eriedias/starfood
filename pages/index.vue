9e4718f50415d0ee7884e6db6aee3f327cfef1dd
Erie Dias <eriedias7@gmail.com>
2021-12-24 20:19:44 -0300

    Food list added

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,37 @@
 <template>
-  <div>
-    Home
-  </div>
+  <section class="food-list">
+    <div class="main-container">
+      <div class="list">
+        <div class="item">
+          <figure>
+            <img src="https://supermercadosrondon.com.br/guiadecarnes/images/postagens/quer_fazer_hamburger_artesanal_perfeito_2019-05-14.jpg">
+          </figure>
+          <div class="text">
+            <h3>Nome da Comida</h3>
+            <span class="price">$19</span>
+          </div>
+        </div>
+        <div class="item">
+          <figure>
+            <img src="https://supermercadosrondon.com.br/guiadecarnes/images/postagens/quer_fazer_hamburger_artesanal_perfeito_2019-05-14.jpg">
+          </figure>
+          <div class="text">
+            <h3>Nome da Comida</h3>
+            <span class="price">$19</span>
+          </div>
+        </div>
+        <div class="item">
+          <figure>
+            <img src="https://supermercadosrondon.com.br/guiadecarnes/images/postagens/quer_fazer_hamburger_artesanal_perfeito_2019-05-14.jpg">
+          </figure>
+          <div class="text">
+            <h3>Nome da Comida</h3>
+            <span class="price">$19</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
